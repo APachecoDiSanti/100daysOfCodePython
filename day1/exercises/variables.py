d37e302b20b5240e1d5fb52f60d9ad5af7340969
Agustín Pacheco Di Santi <pachecodisanti@outlook.com>
2024-01-11 20:41:35 -0300

    Day 1 - Variables

---
+++ day1/exercises/variables.py
@@ -0,0 +1,15 @@
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+copy_of_a = a
+a = b
+b = copy_of_a
+
+
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)

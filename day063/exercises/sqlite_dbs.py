fecdc222bd6fe9e0f954b008e8cdbed42e028750
Agustín Pacheco Di Santi <pachecodisanti@outlook.com>
2024-03-26 20:22:38 -0300

    Day 63 - SQLite DBs

---
+++ day063/exercises/sqlite_dbs.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+
+def main():
+    db = sqlite3.connect("books-collection.db")
+    cursor = db.cursor()
+    cursor.execute("""
+        CREATE TABLE books (
+           id INTEGER PRIMARY KEY,
+           title varchar(250) NOT NULL UNIQUE,
+           author varchar(250) NOT NULL,
+           rating FLOAT NOT NULL
+        )
+    """)
+    cursor.execute("""
+        INSERT INTO books 
+            VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')
+    """)
+    db.commit()
+
+
+
+if __name__ == "__main__":
+    main()
+
